35f92e354cef09b27afe292a585248dd0256444c
stefankreitmayer <stefankreitmayer@gmail.com>
2016-07-09 20:24:13 +0200

    add Svg boilerplate

--- src/View.elm
+++ src/View.elm
@@ -8,4 +8,15 @@ import Subscription exposing (..)
 
 view : Model -> Html Msg
 view model =
-  Html.text model
+  Svg.svg svgAttributes []
+
+
+svgAttributes : List (Attribute Msg)
+svgAttributes =
+  [ width 50
+  , height 50
+  , viewBox <| "0 0 50 50"
+  , property "xmlns:xlink" (Json.string "http://www.w3.org/1999/xlink")
+  , version "1.1"
+  , style "position: absolute; top: 0; left: 0;"
+  ]
